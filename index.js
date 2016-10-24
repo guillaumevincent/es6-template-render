11e975f6f792b04f4b6c6ae0c3bc655e4b14e956
Guillaume Vincent <guillaume@oslab.fr>
2016-10-24 18:15:46 +0200

    change acc to accumulator

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function _recursive_rendering(string, context, stack) {
   return Object.keys(context)
-    .reduce(function (acc, key) {
+    .reduce(function (accumulator, key) {
       var newStack = stack ? stack + '.' : '';
       var find = '\\$\\{\\s*' + newStack + key + '\\s*\\}';
       var re = new RegExp(find, 'g');
 
       if (typeof context[key] === 'object') {
-        return _recursive_rendering(acc, context[key], newStack + key);
+        return _recursive_rendering(accumulator, context[key], newStack + key);
       }
-      return acc.replace(re, context[key]);
+      return accumulator.replace(re, context[key]);
     }, string);
 }
 
